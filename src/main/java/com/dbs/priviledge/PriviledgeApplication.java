5e52f9e0388068c9b0b4a71c8c9acc72511195ec
Ferry L. H <v-ferry.linton@btpn.com>
2019-02-26 01:15:34 +0700

    update jboss deployment

--- src/main/java/com/dbs/priviledge/PriviledgeApplication.java
+++ src/main/java/com/dbs/priviledge/PriviledgeApplication.java
@@ -2,14 +2,21 @@ package com.dbs.priviledge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.dbs.priviledge.config.ApplicationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
-public class PriviledgeApplication {
+public class PriviledgeApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PriviledgeApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PriviledgeApplication.class, args);
 	}
